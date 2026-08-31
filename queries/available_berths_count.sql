a936c5f0505fcaf6b1174f5452089ca1033070b0
MarinaWH <marinawh@local>
2026-08-31 15:48:09 +1200

    Add berth type breakdown query

--- queries/available_berths_count.sql
+++ queries/available_berths_count.sql
@@ -1,14 +1,18 @@
--- Query: Count total number of berths that are available and not decommissioned
+-- Query: Break down available, not-decommissioned berths by berth type
 -- Database: Pacsoft NG (MMS_WH_P)
 -- Environment: READ-ONLY
 
 SELECT
-    COUNT(*) AS TotalAvailableBerths
+    COALESCE(bt.mmbtBerthType, 'Unspecified') AS BerthType,
+    COUNT(*) AS AvailableBerths
 FROM dbo.tmmBerth b
+LEFT JOIN dbo.tmmBerthType bt ON b.mmbeTypePtr = bt.mmbtID
 LEFT JOIN dbo.tmmBerthStatus bs ON b.mmbeStatusPtr = bs.mmbsID
 WHERE b.mmbeActive = 1
   AND b.mmbeDecommissionedDate IS NULL
   AND (
         LOWER(COALESCE(b.mmbeStatus, '')) NOT LIKE '%decommission%'
         OR LOWER(COALESCE(bs.mmbsDescription, '')) NOT LIKE '%decommission%'
-      );
+      )
+GROUP BY COALESCE(bt.mmbtBerthType, 'Unspecified')
+ORDER BY AvailableBerths DESC, BerthType;
